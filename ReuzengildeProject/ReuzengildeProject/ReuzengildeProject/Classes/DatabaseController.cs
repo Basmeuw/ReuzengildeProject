61cca9cc817d6d51487c4be56b2fb7a44fa81e1e
Stef Halmans <stefhalmans@ziggo.nl>
2018-03-26 15:44:14 +0200

    database gefixt

---
+++ ReuzengildeProject/ReuzengildeProject/ReuzengildeProject/Classes/DatabaseController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ReuzengildeProject.Classes
+{
+    public class DatabaseController
+    {
+        // gebruik DatabasseController.SaveJsonLocal(App.Path) om het json file op te slaan in de app
+        public static void SaveJsonLocal(string Path)
+        {
+            SaveFile(Path);
+        }
+        //gebruik JsonToCs reus = DatabaseController.JsonFile(App.Path) om alle informatie uit het opgeslagen json file om te zetten naar in list die zit in her var reus
+        public static JsonToCs JsonFile(string Path)
+        {
+            string file = File.ReadAllText(Path);
+            return JsonToCs.FromJson(file);
+        }
+
+
+        private static async Task SaveFile(string Path)
+        {
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                BasePath = "https://reuzengildetest.firebaseio.com/"
+            };
+            IFirebaseClient client = new FirebaseClient(config);
+            FirebaseResponse response = await client.GetAsync("Reus");
+            var json = response.Body;
+            File.WriteAllText(Path, json);
+        }
+
+    }
+}
---
+++ ReuzengildeProject/ReuzengildeProject/ReuzengildeProject/Classes/JsonToCs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Globalization;
+
+namespace ReuzengildeProject.Classes
+{
+    public partial class JsonToCs
+    {
+        [JsonProperty("reus")]
+        public List<Reus> Reus { get; set; }
+    }
+
+    public partial class Reus
+    {
+        [JsonProperty("Information")]
+        public Information Information { get; set; }
+
+        [JsonProperty("Name")]
+        public Information Name { get; set; }
+    }
+
+    public partial class Information
+    {
+        [JsonProperty("Text")]
+        public string Text { get; set; }
+    }
+
+    public partial class JsonToCs
+    {
+        public static JsonToCs FromJson(string json) => JsonConvert.DeserializeObject<JsonToCs>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this JsonToCs self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
