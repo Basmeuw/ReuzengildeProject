84a948da7bbf81f648b9c2c4dcf4610d852337a4
Stef Halmans <stefhalmans@ziggo.nl>
2018-06-15 16:58:57 +0200

    nu wordt scrollview gereset en als je via het start knopje naar de informatiepagina gaat is de homepagina niet meer geselecteerd. ook zijn de geluidsfragmenten en fotos van de eerste 3 deelnemers toegevoegt

--- ReuzengildeProject/ReuzengildeProject/ReuzengildeProject/Pages/DeelnemersPage.xaml.cs
+++ ReuzengildeProject/ReuzengildeProject/ReuzengildeProject/Pages/DeelnemersPage.xaml.cs
@@ -25,35 +25,5 @@ namespace ReuzengildeProject.Pages
             App.HamburgerPage.DeselectListviewItems();
         }
 	}
-    public class ExtendedScrollView : ScrollView
-    {
-        public event Action<ScrollView, Rectangle> Scrolled;
-
-        public void UpdateBounds(Rectangle bounds)
-        {
-            Position = bounds.Location;
-            if (Scrolled != null)
-                Scrolled(this, bounds);
-        }
-
-        public static readonly BindableProperty PositionProperty =
-            BindableProperty.Create<ExtendedScrollView, Point>(
-                p => p.Position, default(Point));
-
-        public Point Position
-        {
-            get { return (Point)GetValue(PositionProperty); }
-            set { SetValue(PositionProperty, value); }
-        }
-
-        public static readonly BindableProperty AnimateScrollProperty =
-            BindableProperty.Create<ExtendedScrollView, bool>(
-                p => p.AnimateScroll, true);
-
-        public bool AnimateScroll
-        {
-            get { return (bool)GetValue(AnimateScrollProperty); }
-            set { SetValue(AnimateScrollProperty, value); }
-        }
-    }
+    
 }
